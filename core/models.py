5018e06333eb40d95d4a696f6dd3bbea15634960
Wiktor Koźlik <wiktor.kozlik1@gmail.com>
2025-05-16 14:47:31 +0200

    fixed recipes not showing in the recipes.html grid

--- core/models.py
+++ core/models.py
@@ -16,3 +16,28 @@ class Meal(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.calories} kcal)"
+    
+class Recipe(models.Model):
+    MEAL_TYPES = [
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('snack', 'Snack'),
+        ('supper', 'Supper'),
+        ('UNSTOPPABLE MIDNIGHT SWEETS CRAVE', 'UNSTOPPABLE MIDNIGHT SWEETS CRAVE')
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # autor przepisu
+    name = models.CharField(max_length=100)
+    instructions = models.TextField()  # krok po kroku
+    calories = models.PositiveIntegerField()
+    protein = models.PositiveIntegerField()
+    fat = models.PositiveIntegerField()
+    carbs = models.PositiveIntegerField()
+    meal_type = models.CharField(max_length=40, choices=MEAL_TYPES)
+    prep_time = models.PositiveIntegerField(help_text="Time in minutes")  # czas przygotowania
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
